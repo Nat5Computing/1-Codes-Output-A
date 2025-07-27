a45f24a919604bddacef4cdeca838c176b63ac7b
MrsMillerComputing <felicity.miller@ab-ed.org>
2025-07-27 22:28:52 +0100

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+# Display: Hello, Scotland!
+print("Hello, Scotland!")
+
+# Display a sentence that says Python is fun!
+
+
+# Display the name of the person who created Python
+
+
+# Display a line that mentions Odd Socks Day
+
+
+# Display the sentence, Mistakes are proof that you are trying!
